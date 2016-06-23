ba5c66551db1a70a45db9486c93cc19265958f22
ntruiz2015 <naivys.tirado@gmail.com>
2016-06-22 21:56:07 -0400

    Add files via upload
    Initial commit

---
+++ js/quiz.js
@@ -0,0 +1,18 @@
+(function(){
+
+	// script goes here
+    var app = angular.module('myQuiz', []);
+
+    app.controller('QuizController', ['$scope', '$http', '$sce', function($scope,$http,$sce){
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswer = 0;
+        $scope.percentage = 0;
+
+        $http.get('quiz_data.json').then(function(quizData){
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
+    }]);
+
+})();
